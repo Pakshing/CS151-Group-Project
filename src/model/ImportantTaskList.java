5006917702ea8dc717b023d99c86643a39455af0
michael_newman <michaelsn22@yahoo.com>
2020-12-04 12:56:15 -0800

    re-pushing just beginning of adding ImportantTaskList and new view object

---
+++ src/model/ImportantTaskList.java
@@ -0,0 +1,28 @@
+package model;
+
+import java.util.ArrayList;
+
+public class ImportantTaskList
+{
+    private ArrayList<Task> tasksImportant;
+    public ImportantTaskList(){
+        this.tasksImportant = new ArrayList<>();
+    }
+
+    public void addTask(Task task){
+        this.tasksImportant.add(task);
+    }
+
+    public int getSize(){
+        return tasksImportant.size();
+    }
+
+    public Task getTask(int index){
+        return tasksImportant.get(index);
+    }
+
+    public ArrayList<Task> getWholeTasks(){
+        return this.tasksImportant;
+    }
+
+}
